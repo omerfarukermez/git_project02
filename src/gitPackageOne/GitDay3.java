05d4fb75bbeb44a81afaf0d5425a9685392328f3
OmerErmez <omerfe92@gmail.com>
2020-03-15 21:21:49 +0100

    commit

--- src/gitPackageOne/GitDay3.java
+++ src/gitPackageOne/GitDay3.java
@@ -4,9 +4,7 @@ public class GitDay3 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("My mesasage");
-		System.out.println("This is my feature branch");
-		System.out.println("Git Branch Feature");
+
 		
 	}
 }
